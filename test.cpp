5a33de6fb93747724c2922b7224a2f687bd7d24a
2UTM <91062291+2UTM@users.noreply.github.com>
2023-05-08 15:59:21 +0300

    test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 ﻿#include <fstream>
-#pragma warning(disable : 4996) // fopen deprecated
+#include <stdio.h>
+#include "string"
 
-#define BLOCK_SIZE 512 // размер блоков, по которым читаем файл
+#define BLOCK_SIZE 512 // размер блоков, по которым читаем файл (в байтах)
 
 int main()
 {
@@ -10,26 +11,28 @@ int main()
     // Файл результат
     FILE* fout;
 
-    unsigned char buffer[BLOCK_SIZE];
-    if (!(fin = fopen("d:\\labi_C++\\InfoShops_dll\\client\\db.zip", "rb")) == NULL &&
-        !(fout = fopen("d:\\labi_C++\\InfoShops_dll\\client\\db_new.zip", "wb")) == NULL)
+    char buffer[BLOCK_SIZE];
+    #pragma warning(suppress : 4996) // fopen deprecated
+    if (!(fin = fopen("c:\\Install\\Розница_2.3.13.23_чистая.dt", "rb")) == NULL &&
+        #pragma warning(suppress : 4996) // fopen deprecated
+        !(fout = fopen("c:\\Install\\Розница_2.3.13.23_чистая_new.dt", "wb")) == NULL)
     {
 
         // Перемещаем курсор на конец исходного файла
         _fseeki64(fin, 0, SEEK_END);
 
         // Получаем размер исходного файла
-        long long m_file_size = _ftelli64(fin);
+        long long fileSize = _ftelli64(fin);
 
         // Перемещаем курсор на начало исходного файла
         _fseeki64(fin, 0, SEEK_SET);
 
         // Копирование
         // Чтение и запись по байтам "BLOCK_SIZE"
-        for (size_t i = 0; i <= m_file_size / BLOCK_SIZE; ++i)
+        for (size_t i = 0; i <= fileSize / BLOCK_SIZE; ++i)
         {
-            int readed = fread(buffer, sizeof(unsigned char), BLOCK_SIZE, fin);
-            fwrite(buffer, sizeof(unsigned char), readed, fout);
+            int readed = fread(buffer, sizeof(char), BLOCK_SIZE, fin);
+            fwrite(buffer, sizeof(char), readed, fout);
         }
         fclose(fin);
         fclose(fout);
